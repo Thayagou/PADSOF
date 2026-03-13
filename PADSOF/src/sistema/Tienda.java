4725bac9f7ae6e0e96e4f5c424a8942f5166ef5e
JuanIV <jibanezvilla@gmail.com>
2026-03-13 13:39:06 +0100

    Dar de alta/baja + getEmpleado/Cliente

--- PADSOF/src/sistema/Tienda.java
+++ PADSOF/src/sistema/Tienda.java
@@ -18,8 +18,28 @@ public class Tienda {
 		usuarios.put("GESTOR", new Gestor("GESTOR", "GESTOR123"));
 	}
 	
-	public Usuario getGestor() {
-		return usuarios.get("GESTOR");
+	public Gestor getGestor() {
+		return (Gestor)usuarios.get("GESTOR");
+	}
+	
+	public Usuario getUsuario(String nombre) {
+		return usuarios.get(nombre);
+	}
+	
+	public Empleado getEmpleado(String nombre) {
+		Usuario usr = usuarios.get(nombre);
+		if(usr instanceof Empleado) {
+			return (Empleado)usr;
+		}
+		return null;
+	}
+	
+	public ClienteRegistrado getCliente(String nombre) {
+		Usuario usr = usuarios.get(nombre);
+		if(usr instanceof ClienteRegistrado) {
+			return (ClienteRegistrado)usr;
+		}
+		return null;
 	}
 	
 	public boolean registrarse(String nombre, String contrasena, String confirmarContrasena) {
@@ -32,8 +52,21 @@ public class Tienda {
 	}
 	
 	public Usuario iniciarSesion(String nombre, String contrasena) {
+		if(!usuarios.containsKey(nombre)) return null;
 		if(usuarios.get(nombre).getContrasena().equals(contrasena))
 			return usuarios.get(nombre);
 		return null;
+	}
+	
+	public boolean darDeAltaEmpleado(String nombre, String contrasena, Permiso...permisos) {
+		if(usuarios.containsKey(nombre)) return false;
+		usuarios.put(nombre, new Empleado(nombre, contrasena, permisos));
+		return true;
+	}
+	
+	public boolean darDeBajaEmpleado(String nombre) {
+		if(getEmpleado(nombre) == null) return false;
+		usuarios.remove(nombre);
+		return true;
 	}
 }
