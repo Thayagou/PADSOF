e809ad815099cfabba3ec9d99325ea3c8d8ac084
JuanIV <jibanezvilla@gmail.com>
2026-04-06 15:57:05 +0200

    ProductoNoDisponible fix

--- PADSOF/src/exceptions/ProductoNoDisponibleException.java
+++ PADSOF/src/exceptions/ProductoNoDisponibleException.java
@@ -2,14 +2,28 @@ package exceptions;
 
 import venta.productos.*;
 
-public class ProductoNoDisponibleException extends Exception {
+/**
+ * Excepción que se lanza cuando no hay unidades disponibles de un producto
+ */
+public class ProductoNoDisponibleException extends CustomException {
+	private static final long serialVersionUID = 1L;
+	/** Producto del que no quedan unidades */
 	private Producto producto;
 	
+	/**
+	 * Creador de la excepción
+	 * @param message Mensaje de error
+	 * @param p Producto que se intentó adquirir
+	 */
 	public ProductoNoDisponibleException(String message, Producto p) {
 		super(message);
 		this.producto = p;
 	}
 	
+	/**
+	 * Getter del producto que causó la excepción
+	 * @return Producto que no estaba disponible
+	 */
 	public Producto getProducto() {
 		return producto;
 	}
