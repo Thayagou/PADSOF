4e4027ada55d096c58aae6b468aab10146ba461e
Thayagou <tiagooselka@gmail.com>
2026-04-15 18:52:10 +0200

    Merge branch 'main' of https://github.com/Thayagou/PADSOF.git into main

--- src/gui/TiendaFrame.java
+++ src/gui/TiendaFrame.java
@@ -2,7 +2,10 @@ package gui;
 
 import javax.swing.JFrame;
 
+import sistema.Tienda;
+
 public class TiendaFrame extends JFrame {
+	
 	public TiendaFrame() {
 		setTitle("Android's Dungeon");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
