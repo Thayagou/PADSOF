84a30a9c97f79540b5ced4b93be495f97bcafd3a
Thayagou <tiagooselka@gmail.com>
2026-04-29 00:27:00 +0200

    Intermedio3

--- src/controladores/ControlAnadirDescuento.java
+++ src/controladores/ControlAnadirDescuento.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
