459ceaa3fcfa00bf9816627b1e8a58166809663c
Thayagou <tiagooselka@gmail.com>
2026-05-02 21:28:30 +0200

    Merge

--- src/controladores/noRegistrado/ControlInicioSinRegistrar.java
+++ src/controladores/noRegistrado/ControlInicioSinRegistrar.java
@@ -13,8 +13,8 @@ import vistas.noRegistrado.*;
 public class ControlInicioSinRegistrar implements ActionListener {
 
 	private Tienda tienda;
-	//private VentanaInicioSinRegistrar vista;
-	private TestVentanaInicio vista;
+	private VentanaInicioSinRegistrar vista;
+	//private TestVentanaInicio vista;
 	
 	public ControlInicioSinRegistrar(Tienda tienda) {
 		this.tienda = tienda;
@@ -32,12 +32,12 @@ public class ControlInicioSinRegistrar implements ActionListener {
 		// Obtener productos populares del modelo y construir la vista
 		Producto[] populares = tienda.getAlmacen().getProductosCoincidentes("");
 
-		//this.vista = new VentanaInicioSinRegistrar();
-		this.vista = new TestVentanaInicio();
+		this.vista = new VentanaInicioSinRegistrar();
+		//this.vista = new TestVentanaInicio();
 		//this.vista.setClickListener(this);
 		for(Producto p : populares) {
-			//new ControlPanelProductoNoRegistrado(tienda, p, vista);
-			new ControladorTest(tienda, p, vista);
+			new ControlPanelProductoNoRegistrado(tienda, p, vista);
+			//new ControladorTest(tienda, p, vista);
 		}
 		tiendaFrame.setVistaActual(vista);
 	}
