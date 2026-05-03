8bdf8afa8ae661790ace557041599115bad31413
Thayagou <tiagooselka@gmail.com>
2026-05-04 00:38:35 +0200

    Arreglillo

--- src/controladores/empleado/ControlPanelProductoGestionar.java
+++ src/controladores/empleado/ControlPanelProductoGestionar.java
@@ -1,5 +1,39 @@
 package controladores.empleado;
 
-public class ControlPanelProductoGestionar {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import modelo.sistema.Tienda;
+import modelo.venta.productos.Categoria;
+import modelo.venta.productos.Producto;
+import vistas.common.PanelCategoria;
+import vistas.common.VentanaConDisplay;
+import vistas.empleado.PanelCategoriaGestionarCategoria;
+import vistas.empleado.PanelProductoGestionarProducto;
+
+public class ControlPanelProductoGestionar implements ActionListener {
+	private Producto producto;
+	private Tienda tienda;
+	private PanelProductoGestionarProducto panel;
+	
+	public ControlPanelProductoGestionar(Tienda tienda, Producto producto, VentanaConDisplay<? super PanelCategoria> vista) {
+		this.tienda = tienda;
+		this.producto = producto;
+		
+		panel = new PanelProductoGestionarProducto(producto.getNombre());
+		panel.setControlador(this);
+		
+		vista.anadirDisplay(panel);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case PanelCategoriaGestionarCategoria.BORRAR_ACTION:
+			break;
+		case PanelCategoriaGestionarCategoria.MODIFICAR_ACTION:
+			break;
+		}
+	}
 
 }
--- src/vistas/empleado/PanelArticuloPendienteValoracion.java
+++ src/vistas/empleado/PanelArticuloPendienteValoracion.java
@@ -6,7 +6,7 @@ public class PanelArticuloPendienteValoracion extends PanelArticulo{
 	private static final long serialVersionUID = 1L;
 
 	public PanelArticuloPendienteValoracion (String nombreUsuario, String fotoDePerfil,  String nombre, String descripcion, String interesadoEn, double estimacion, String estado, String actionName, String...categorias) {
-		super(nombreUsuario, fotoDePerfil, nombre, descripcion, interesadoEn, estimacion, estado, actionName, categorias);
+		super(nombre, descripcion, interesadoEn, estimacion, estado, actionName, categorias);
 		
 		if (estimacion < 0) inicializarBoton("Valorar");
 	}
