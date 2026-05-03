aeac463241342f5c024a3dc91203a31d275828d1
Thayagou <tiagooselka@gmail.com>
2026-05-04 00:39:28 +0200

    Arreglillo2

--- src/vistas/empleado/PanelArticuloPendienteValoracion.java
+++ src/vistas/empleado/PanelArticuloPendienteValoracion.java
@@ -5,7 +5,7 @@ import vistas.common.PanelArticulo;
 public class PanelArticuloPendienteValoracion extends PanelArticulo{
 	private static final long serialVersionUID = 1L;
 
-	public PanelArticuloPendienteValoracion (String nombreUsuario, String fotoDePerfil,  String nombre, String descripcion, String interesadoEn, double estimacion, String estado, String actionName, String...categorias) {
+	public PanelArticuloPendienteValoracion (String nombre, String descripcion, String interesadoEn, double estimacion, String estado, String actionName, String...categorias) {
 		super(nombre, descripcion, interesadoEn, estimacion, estado, actionName, categorias);
 		
 		if (estimacion < 0) inicializarBoton("Valorar");
